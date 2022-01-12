7059d6bc3e5739f62c01654ed3368242a2fcd964
samuel <samuelmmanor@gmail.com>
2022-01-12 10:42:04 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(win) {
+    let year = win.year;
+    if (win.result === "W") {
+      return win.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  const finder = record.find(superbowlWin);
